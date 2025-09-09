bc9ed7b0054dfd07bcda8edd62c41face908220f
Caner Yuksel <Cyuksel@gemak.local>
2025-09-09 18:02:05 +0300

    Implementation of Lazy Loading2

--- Downloads/sap.ui.core.tutorial.navigation.02 (1)/webapp/controller/employee/Resume.controller.js
+++ Downloads/sap.ui.core.tutorial.navigation.02 (1)/webapp/controller/employee/Resume.controller.js
@@ -1,19 +1,22 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+	"sap/ui/demo/nav/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
 	"use strict";
+	var _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 
 	return BaseController.extend("sap.ui.demo.nav.controller.employee.Resume", {
 
 		onInit: function () {
 			var oRouter = this.getRouter();
 
+			this.getView().setModel(new JSONModel(), "view");
 			oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
 
 		},
 
 		_onRouteMatched : function (oEvent) {
-			var oArgs, oView;
+			var oArgs, oView, oQuery;
 
 			oArgs = oEvent.getParameter("arguments");
 			oView = this.getView();
@@ -30,6 +33,24 @@ sap.ui.define([
 					}
 				}
 			});
+
+			oQuery = oArgs["?query"];
+			if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1){
+				oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);
+				// support lazy loading for the hobbies and notes tab
+				if (oQuery.tab === "Hobbies" || oQuery.tab === "Notes"){
+					// the target is either "resumeTabHobbies" or "resumeTabNotes"
+					this.getRouter().getTargets().display("resumeTab" + oQuery.tab);
+				}
+			} else {
+				// the default query param should be visible at all time
+				this.getRouter().navTo("employeeResume", {
+					employeeId : oArgs.employeeId,
+					"?query": {
+						tab : _aValidTabKeys[0]
+					}
+				}, true /*no history*/);
+			}
 		},
 
 		_onBindingChange : function (oEvent) {
@@ -38,6 +59,11 @@ sap.ui.define([
 				this.getRouter().getTargets().display("notFound");
 			}
 		},
+
+		/**
+		 * We use this event handler to update the hash in case a new tab is selected.
+		 * @param oEvent
+		 */
 		onTabSelect : function (oEvent){
 			var oCtx = this.getView().getBindingContext();
 
