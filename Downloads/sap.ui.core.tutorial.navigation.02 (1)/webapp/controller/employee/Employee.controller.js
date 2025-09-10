a2123051ac8f9ea7dfbf096b68f81b7f56361664
Caner Yuksel <Cyuksel@gemak.local>
2025-09-10 11:37:04 +0300

    Employee Overview Page Eklendi

--- Downloads/sap.ui.core.tutorial.navigation.02 (1)/webapp/controller/employee/Employee.controller.js
+++ Downloads/sap.ui.core.tutorial.navigation.02 (1)/webapp/controller/employee/Employee.controller.js
@@ -2,21 +2,28 @@ sap.ui.define([
 	"sap/ui/demo/nav/controller/BaseController"
 ], function (BaseController) {
 	"use strict";
+
 	return BaseController.extend("sap.ui.demo.nav.controller.employee.Employee", {
+
 		onInit: function () {
 			var oRouter = this.getRouter();
+
 			oRouter.getRoute("employee").attachMatched(this._onRouteMatched, this);
+
 			// Hint: we don't want to do it this way
 			/*
-			oRouter.attachRouteMatched(function (oEvent){
-				var sRouteName, oArgs, oView;
-				sRouteName = oEvent.getParameter("name");
-				if (sRouteName === "employee"){
-					this._onRouteMatched(oEvent);
-				}
-			}, this);
-			*/
+			 oRouter.attachRouteMatched(function (oEvent){
+				 var sRouteName, oArgs, oView;
+
+				 sRouteName = oEvent.getParameter("name");
+				 if (sRouteName === "employee"){
+				 	this._onRouteMatched(oEvent);
+				 }
+			 }, this);
+			 */
+
 		},
+
 		_onRouteMatched : function (oEvent) {
 			var oArgs, oView;
 			oArgs = oEvent.getParameter("arguments");
@@ -35,6 +42,7 @@ sap.ui.define([
 				}
 			});
 		},
+
 		_onBindingChange : function (oEvent) {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
@@ -49,5 +57,7 @@ sap.ui.define([
 				employeeId : oCtx.getProperty("EmployeeID")
 			});
 		}
+
 	});
+
 });
