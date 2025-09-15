1510e41a66dfb0367b69675dc0d3595436c8cddc
Caner Yuksel <Cyuksel@gemak.local>
2025-09-15 17:33:27 +0300

    Data binding Basics

--- Downloads/sap.ui.core.tutorial.databinding.01/webapp/Component.js
+++ Downloads/sap.ui.core.tutorial.databinding.01/webapp/Component.js
@@ -1,20 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/model/BindingMode"
-], (UIComponent,BindingMode) => {
+	"sap/ui/core/UIComponent"
+], (UIComponent) => {
 	"use strict";
 
 	return UIComponent.extend("ui5.databinding.Component", {
 		metadata: {
 			interfaces: ["sap.ui.core.IAsyncContentCreation"],
 			manifest: "json"
-		},
-
-		init() {
-			// call the init function of the parent
-			UIComponent.prototype.init.apply(this, arguments);
-
-			this.getModel().setDefaultBindingMode(BindingMode.OneWay);
 		}
 	});
 });
